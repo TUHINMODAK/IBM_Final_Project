43ac61a6be95ea2c0a05b2394358a8864524c6fa
Suprabhat Maiti <maitisuprabhat@gmail.com>
2026-06-13 20:21:47 +0530

    feat(database): add initial migration and create initial tables

---
+++ MovieAPI/Migrations/20260613144814_Inital.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inital : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Poster_Link = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Series_Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Released_Year = table.Column<int>(type: "int", nullable: false),
+                    Certificate = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Runtime = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IMDB_Rating = table.Column<double>(type: "float", nullable: true),
+                    Overview = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    No_of_Votes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ MovieAPI/Model/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieAPI.Model;
+
+namespace MovieAPI.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieList> Movies { get; set; }
+    }
+}
