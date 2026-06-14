bcdeeccd5a3434e1bbd4693b742483f29cef3a00
Suprabhat Maiti <maitisuprabhat@gmail.com>
2026-06-14 23:27:54 +0530

    Refactor Homepage and add fetch request

--- movieui/src/Components/MovieCard.jsx
+++ movieui/src/Components/MovieCard.jsx
@@ -1,19 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class MovieCard extends Component {
   render() {
+    const { poster, name, rating } = this.props;
+
     return (
-      <div className='col'>
-        <div className='overflow-hidden'>
-        <img src={this.props.poster} alt="image" />
-        </div>
-        <div>
-        {this.props.name}
-        {this.props.rating}
+      <div className="col">
+        <div className="card h-100 shadow-sm border-0">
+
+          <img
+            src={poster}
+            alt={name}
+            className="card-img-top"
+            style={{ height: "350px", objectFit: "contain" }}
+          />
+
+          <div className="card-body text-center">
+            <h5 className="card-title fw-bold">{name}</h5>
+
+            <span className="badge bg-warning text-dark fs-6">
+              ⭐ {rating}
+            </span>
+          </div>
+
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default MovieCard
+export default MovieCard;
