9f001ddcd48c08127b4936b789c8213c2923c841
Suprabhat Maiti <maitisuprabhat@gmail.com>
2026-06-16 11:40:58 +0530

    fix add no movie found

--- movieui/src/Components/Admin_Dashboard/AdminDashboard.jsx
+++ movieui/src/Components/Admin_Dashboard/AdminDashboard.jsx
@@ -135,19 +135,17 @@ export class AdminDashboard extends Component {
                         </tbody>)}
                     </table>
                     {/* Pagination */}
-                    <nav className="mt-4">
+                    <nav aria-label="...">
                         <ul className="pagination justify-content-center">
+                            <li className="page-item"><a href="#" className="page-link">Previous</a></li>
+                            <li className="page-item"><a className="page-link" href="#">1</a></li>
                             <li className="page-item active">
-                                <a className="page-link">1</a>
+                            <a className="page-link" href="#" aria-current="page">2</a>
                             </li>
-                            <li className="page-item">
-                                <a className="page-link">2</a>
-                            </li>
-                            <li className="page-item">
-                                <a className="page-link">3</a>
-                            </li>
+                            <li className="page-item"><a className="page-link" href="#">3</a></li>
+                            <li className="page-item"><a className="page-link" href="#">Next</a></li>
                         </ul>
-                    </nav>
+                        </nav>
                 </div>
             </div>
         </div>
